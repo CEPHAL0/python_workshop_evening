92d8d232ad4487bbfdee6a74600bf02803877a9e
CEPHAL0 <xaradxarma@gmail.com>
2025-10-16 18:41:35 +0545

    Virtual Environment Codes

---
+++ Day12/virtual_environment.py
@@ -0,0 +1,11 @@
+import pandas
+
+# change the directory to the folder of project: cd Day12
+# Create a virtual environment -> python -m venv .venv
+# Activate the environment - 
+    # mac -> source .venv/bin/activate
+    # windows -> .venv/bin/activate
+# Check the installed packages -> pip list
+# Install a package in venv -> pip install <package_name>
+# Check the newly installed packages -> pip list
+# Deactivate the environment -> deactivate
